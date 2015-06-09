cf6bfd17243ddfb0c163d198e3ae99b667f2c80c
Bret Schuring <bret.schuring@gmail.com>
2015-06-09 00:08:17 -0500

    Removed Android support.

--- src/test/groovy/com/jimdo/gradle/AptPluginSpec.groovy
+++ src/test/groovy/com/jimdo/gradle/AptPluginSpec.groovy
@@ -27,31 +27,4 @@ class AptPluginSpec extends Specification {
     new File(aptBasePath).exists()
     new File("$aptBasePath/coffee").listFiles().length > 0
   }
-
-  def "it should generate sources for android project"() {
-    def projectPath = 'src/test/build-tests/android-project'
-    def aptBasePath = "$projectPath/build/source/apt"
-
-    given:
-    ProjectConnection connection = GradleConnector.newConnector()
-      .forProjectDirectory(new File(projectPath)).connect()
-
-    when:
-    try {
-      connection.newBuild().forTasks("clean", "build").run()
-    } finally {
-      connection.close()
-    }
-
-    then:
-    new File(aptBasePath).exists()
-    new File("$aptBasePath/debug/com/example/dagger/simple").listFiles().length > 0
-    new File("$aptBasePath/debug/com/example/dagger/simple/ui").listFiles().length > 0
-    new File("$aptBasePath/release/com/example/dagger/simple").listFiles().length > 0
-    new File("$aptBasePath/release/com/example/dagger/simple/ui").listFiles().length > 0
-  }
-
-  def "it should generate sources for android-library project"() {
-    // TODO: does this even make sense?!
-  }
 }
